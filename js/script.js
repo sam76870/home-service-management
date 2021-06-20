a0f66b436d2b62a0f31cddf94bf67523b38527d4
Shubham Pathak <sam76870@gmail.com>
2021-06-20 19:41:30 +0530

    push upto icon container

--- js/script.js
+++ js/script.js
@@ -5,11 +5,3 @@ toggleButton.addEventListener("click",() =>{
     navbarLinks.classList.toggle("active");
 })
 
-var counter = 1;
-setInterval(function () {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 3) {
-        counter = 1;
-    }
-}, 3000);
