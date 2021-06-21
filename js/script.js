a2e41bf3e033de14afff36a2d2853ef8202e3c72
Shubham Pathak <sam76870@gmail.com>
2021-06-21 13:01:29 +0530

    push upto about page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 let toggleButton = document.getElementsByClassName("toggle_button")[0];
 let navbarLinks = document.getElementsByClassName("navbar_links")[0];
+let counters = document.querySelectorAll(".counter");
+let speed = 1000;
+counters.forEach(counter => {
+    let updateCounter = () => {
+        let target = +counter.getAttribute('data-target');
+        let count = +counter.innerText;
 
-toggleButton.addEventListener("click",() =>{
+        let inc = target / speed;
+        console.log(count);
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCounter, 1);
+        } else {
+            count.innerText = target;
+        }
+    }
+    updateCounter();
+})
+toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active");
 })
 
